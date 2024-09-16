ecc700b83094353fa5e46acd85489d9f56938f55
arocha <alberto.jmas.rocha@gmail.com>
2024-09-16 18:01:03 +0100

    asa

--- chapters/GDM.R
+++ chapters/GDM.R
@@ -22,7 +22,9 @@ alpha = 0.5
 beta = 0.5
 k = 0.01
 N0 = 20000
-P = list(48)
+P = list(48,49)
+P.ini = list(10000,15000)
+
 
 # Vectores 
 
@@ -32,18 +34,45 @@ e_t = df_effort %>%
   as.vector() %>%
   unlist()
 
-C_i = rep(0,52)
-C_i[unlist(P)] = 1
-C_i
-t = 1:52
+C_t = df_effort %>% 
+  filter(year_sale == 1995) %>% 
+  select(catch) %>%
+  as.vector() %>%
+  unlist()
 
+I_i = rep(0,52)
+I_i[unlist(P)] = 1
+I_i
+R_j = rep(0,52)
+
+contador = 1
+for(i in 1:52){
+  if(I_i[i] == 1) {
+    R_j[i] = P.ini[contador]
+    contador = contador+1
+  }}
+
+#simulacao: t0
+
+# Componente das capturas:
+
+for(i in 1:52){
+  res = C_t[i-1]*exp(-M*())
+}
+
+
+for(t_i in 1:52){
 k*(e_t^alpha)*m* ( 
-  N0 ^ (M * t) - m * (
-    sum(C_i * exp(-M*(t-i-1)))) + 
-    sum(I_i * R_j * exp(-M*(t-tau_j))))^beta
+  N0 ^ (M * t_i) - m * (
+    sum(C_t[t_i-1] * exp(-M*(t_i-1)))) + 
+    sum(I_i[t_i] * R_j[t_i] * exp(-M*(t_i-tau_j))))^beta
     
+}
+
+
+
   
   
-  
+
   
 c(4,0)  
